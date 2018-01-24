1a76183d898aa4a845733305997abc04834932b3
Chantal <chantal.pellegrini@gmail.com>
2018-01-24 21:40:27 +0100

    Initial commit

---
+++ coding.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Chantal on 24.01.2018.
+//
+#include "data.h"
+
+void decode(char input[]){
+
+}
+
+void encode(char input[]){
+
+}
+
---
+++ vigenere.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstring>
+#include "coding.cpp"
+
+int main() {
+    bool end = false;
+    while (!end) {
+        printf("Do you want to encode or decode? Choose E or D. If you want to quit please press Q.\n");
+        char mode[2];
+        fgets(mode, 2, stdin);
+        fflush(stdin);
+        while(strcmp(mode, "D") && strcmp(mode, "d") && strcmp(mode, "E") && strcmp(mode, "e") && strcmp(mode, "Q") && strcmp(mode, "q")){
+            printf("No valid input. Please choose again.\n");
+            fgets(mode, 2, stdin);
+            fflush(stdin);
+        }
+
+        if (!strcmp(mode, "D") || !strcmp(mode, "d"))
+        {
+            printf("Please type the text you want to decode: (until 127 characters)\n");
+            char input[128];
+            fgets(input, 128, stdin);
+            fflush(stdin);
+            decode(input);
+        }
+        else if (!strcmp(mode, "E") || !strcmp(mode, "e"))
+        {
+            printf("Please type the text you want to decode: (until 128 characters)\n");
+            char input[128];
+            fgets(input, 128, stdin);
+            fflush(stdin);
+            encode(input);
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
